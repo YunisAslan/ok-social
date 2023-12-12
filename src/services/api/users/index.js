1e75fcfd28493d03ff53b128a2ab163dc36d57b1
yunisaslanov397@gmail.com <yunisaslanov397@gmail.com>
2023-12-12 12:13:59 +0400

    fix: try to fix deployment error

--- src/services/api/users/index.js
+++ src/services/api/users/index.js
@@ -1,28 +1,27 @@
 import axios from "axios";
 
+const BASE_URL = "https://656e3ec6bcc5618d3c24b6a6.mockapi.io";
+
 export async function getAllUsers() {
-  const { data } = await axios.get(`${process.env.BASE_URL}/users`);
+  const { data } = await axios.get(`${BASE_URL}/users`);
 
   return data;
 }
 
 export async function getUserByID(id) {
-  const { data } = await axios.get(`${process.env.BASE_URL}/users/${id}`);
+  const { data } = await axios.get(`${BASE_URL}/users/${id}`);
 
   return data;
 }
 
 export async function createUserRequest(payload) {
-  const { data } = await axios.post(`${process.env.BASE_URL}/users`, payload);
+  const { data } = await axios.post(`${BASE_URL}/users`, payload);
 
   return data;
 }
 
 export async function editUserRequest(id, payload) {
-  const { data } = await axios.put(
-    `${process.env.BASE_URL}/users/${id}`,
-    payload
-  );
+  const { data } = await axios.put(`${BASE_URL}/users/${id}`, payload);
 
   return data;
 }
